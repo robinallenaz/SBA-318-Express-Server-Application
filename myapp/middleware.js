fd3b4fe4137d6edce503443b5d9910873944831e
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-27 20:12:35 -0700

    Added extra console logs to middleware

--- myapp/middleware.js
+++ myapp/middleware.js
@@ -1,10 +1,14 @@
 //middleware.js
 const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
+  //Console log to see if working
+  console.log('Inside loggerMiddleware');
+  console.log(`${req.method} ${req.url}`);
     next();
   };
   
   const authMiddleware = (req, res, next) => {
+    //Console log to see if working
+    console.log('Inside authMiddleware');
     const authToken = req.header('Authorization');
     if (!authToken) {
       return res.status(401).send('Unauthorized');
