e5474535c4db25b347837c50656217ade659a254
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-11-06 18:48:31 -0700

    Updated errorHandler

--- errorHandler.js
+++ errorHandler.js
@@ -2,6 +2,7 @@
 const errorHandler = (error, req, res, next) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
+    next(error);
   };
   
   module.exports = errorHandler;
